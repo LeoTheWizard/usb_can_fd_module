339aadb38a7318d7f4e6cfc62f9c529110d7e1e9
LeoTheWizard <leowalk183@gmail.com>
2026-05-15 20:12:34 +0100

    initial project structure

---
+++ src/rp2354a_firmware/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author Leo Walker
+ *
+ * @brief Main entry point for the RP2354A firmware.
+ *
+ * @copyright (c) 2025 Leo Walker @ MIT License @cite MIT License
+ *
+ */
+
+#include <stdlib.h>
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
